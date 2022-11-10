771214c3cebb65eecb716ee83e3bb0b1b8fbc336
Dani Vicario <danigv.email@gmail.com>
2022-11-10 17:26:21 +0100

    added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { motion, useMotionValue, useTransform } from "framer-motion";
+import { useState } from "react";
 
 const StyledHome = styled(motion.div)`
   /* [data-styled-component-toggle] {
@@ -12,83 +13,27 @@ const StyledHome = styled(motion.div)`
   width: 100%;
   flex-wrap: wrap;
   overflow: hidden;
+  flex-direction: column;
+
+  button {
+    margin-top: 100px;
+  }
 `;
 
-const StyledHomeC = styled(motion.div)`
-  /* [data-styled-component-toggle] {
-    margin-bottom: 10px;
-  } */
-
-  display: flex;
-  max-width: 100vw;
-  max-height: 100vh;
-  width: 100%;
-  flex-wrap: wrap;
-  overflow: hidden;
+const Circle = styled.div`
+  height: 300px;
+  width: 300px;
+  border-radius: 50%;
+  background: white;
 `;
-
-function randomFloat(min: number, max: number) {
-  return Math.random() * (max - min) + min;
-}
-function randomInt(min: number, max: number) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-function shuffle(array: any[]) {
-  return array.sort(() => Math.random() - 0.5);
-}
-
-const spring = {
-  // type: "spring",
-  damping: 10,
-  stiffness: 100,
-  delay: 2,
-  duration: 2
-};
-const boxVariant = {
-  hidden: {
-    x: "-100vw" //move out of the site
-  },
-  visible: {
-    x: 0, // bring it back to nrmal
-    transition: {
-      delayChildren: 1,
-      staggerChildren: 0.2 //apply stagger on the parent tag
-    }
-  }
-};
-
-const listVariant = {
-  hidden: {
-    opacity: 0,
-    height: 20,
-    width: 20,
-    background: "red"
-  },
-  visible: {
-    x: 0, // bring it back to nrmal
-    opacity: 1,
-    height: 100,
-    width: 20,
-    background: "red"
-  }
-};
 
 export default function App() {
-  const XX = Array(20)
-    .fill(0)
-    .map((x, i) => (
-      <motion.div variants={listVariant} key={i}>
-        x
-      </motion.div>
-    ));
+  const [visible, setVisible] = useState(true);
 
   return (
     <StyledHome>
-      {XX.length > 0 && (
-        <StyledHomeC variants={boxVariant} initial="hidden" animate="visible">
-          {XX}
-        </StyledHomeC>
-      )}
+      {visible && <Circle />}
+      {<button onClick={() => setVisible(!visible)}>Invisible</button>}
     </StyledHome>
   );
 }
