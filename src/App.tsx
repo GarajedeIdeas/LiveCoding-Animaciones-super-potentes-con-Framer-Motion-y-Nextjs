f6e5b9e392ab335b0425dad1e7daf46e16f56da5
Dani Vicario <danigv.email@gmail.com>
2022-12-14 08:17:35 +0100

    airbnb

--- src/App.tsx
+++ src/App.tsx
@@ -1,94 +1,52 @@
+import { motion, Variants } from "framer-motion";
 import styled from "styled-components";
-import { motion, useMotionValue, useTransform } from "framer-motion";
+import "./reset.css";
+import Logo from "./Logo";
 
-const StyledHome = styled(motion.div)`
-  /* [data-styled-component-toggle] {
-    margin-bottom: 10px;
-  } */
-
+const Container = styled(motion.ul)`
+  height: 100vh;
+  background: #ccc;
   display: flex;
-  max-width: 100vw;
-  max-height: 100vh;
-  width: 100%;
-  flex-wrap: wrap;
-  overflow: hidden;
+  justify-content: center;
+  align-items: center;
 `;
 
-const StyledHomeC = styled(motion.div)`
-  /* [data-styled-component-toggle] {
-    margin-bottom: 10px;
-  } */
-
-  display: flex;
-  max-width: 100vw;
-  max-height: 100vh;
-  width: 100%;
-  flex-wrap: wrap;
-  overflow: hidden;
-`;
-
-function randomFloat(min: number, max: number) {
-  return Math.random() * (max - min) + min;
-}
-function randomInt(min: number, max: number) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-function shuffle(array: any[]) {
-  return array.sort(() => Math.random() - 0.5);
-}
-
-const spring = {
-  // type: "spring",
-  damping: 10,
-  stiffness: 100,
-  delay: 2,
-  duration: 2
-};
-const boxVariant = {
-  hidden: {
-    x: "-100vw" //move out of the site
-  },
+const logosVariants: Variants = {
+  hidden: { opacity: 0 },
   visible: {
-    x: 0, // bring it back to nrmal
+    opacity: 1,
     transition: {
-      delayChildren: 1,
-      staggerChildren: 0.2 //apply stagger on the parent tag
+      duration: 2,
+      staggerChildren: 1
     }
   }
 };
 
-const listVariant = {
-  hidden: {
-    opacity: 0,
-    height: 20,
-    width: 20,
-    background: "red"
-  },
+const logoVariant: Variants = {
+  whileTap: { scale: 2, rotateX: 180 },
+  hidden: { opacity: 0, y: 0 },
   visible: {
-    x: 0, // bring it back to nrmal
+    rotate: 360,
+    y: 0,
     opacity: 1,
-    height: 100,
-    width: 20,
-    background: "red"
+    transition: {
+      duration: 0.2
+    }
   }
 };
 
 export default function App() {
-  const XX = Array(20)
-    .fill(0)
-    .map((x, i) => (
-      <motion.div variants={listVariant} key={i}>
-        x
-      </motion.div>
-    ));
-
   return (
-    <StyledHome>
-      {XX.length > 0 && (
-        <StyledHomeC variants={boxVariant} initial="hidden" animate="visible">
-          {XX}
-        </StyledHomeC>
-      )}
-    </StyledHome>
+    <Container variants={logosVariants} initial="hidden" animate="visible">
+      <motion.li key={1} variants={logoVariant} whileTap="whileTap">
+        <Logo height="300" whileHover={{ opacity: 0 }} />
+      </motion.li>
+      <motion.li key={2} variants={logoVariant} whileTap="whileTap">
+        <Logo height="400" />
+      </motion.li>
+      <motion.li key={3} variants={logoVariant} whileTap="whileTap">
+        <Logo height="500" />
+      </motion.li>
+    </Container>
   );
 }
